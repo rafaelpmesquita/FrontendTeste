4da0d7a495b2ec521473a1d418810606a7e042ba
rafael.mesquita <rafael.mesquita@animaeducacao.com.br>
2024-01-25 00:12:53 -0300

    remoçao itens nao utilizados

--- src/commom/constantes/ConstantesGeral.ts
+++
@@ -1,7 +0,0 @@
-export class ConstantesGeral {
-
-    public static ENTRADA_SAIDA = [
-        { descricao: 'Saida', codigo: 'S' },
-        { descricao: 'Entrada', codigo: 'E' },
-    ];
-}
--- src/commom/loader/index.ts
+++
@@ -1,6 +0,0 @@
-import _Vue from 'vue';
-import LoaderInstance from './LoaderInstance';
-
-export const LoaderPlugin = (Vue: typeof _Vue) => {
-  Vue.prototype.$loader = new LoaderInstance();
-};
