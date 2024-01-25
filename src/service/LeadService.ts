9bc7b5e97eafa058d6bc27682c19aea788120ef2
rafael.mesquita <rafael.mesquita@animaeducacao.com.br>
2024-01-25 00:13:06 -0300

    remover console.log

--- src/service/LeadService.ts
+++ src/service/LeadService.ts
@@ -2,14 +2,11 @@ import LeadModel from '@/models/LeadModel';
 import axios from 'axios';
 export class LeadService {
     public async getInvited(request:string) {
-        console.log(request)
         const response = await axios.get('https://localhost:7156/api/Lead' + request);
         return response.data;
     }
     public async getAccepted(request:string) {
-        console.log(request)
         const response = await axios.get('https://localhost:7156/api/Lead'+request+'/accepted');
-        console.log(response)
         return response.data;
     }
     public async changeStatus(request: LeadModel,endpoint:string) {
