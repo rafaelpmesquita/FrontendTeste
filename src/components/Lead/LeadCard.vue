456d9f9058af625c26d4c683fc2504c1e7bad11d
rafael.mesquita <rafael.mesquita@animaeducacao.com.br>
2024-01-25 09:12:21 -0300

    Formatação data

--- src/components/Lead/LeadCard.vue
+++ src/components/Lead/LeadCard.vue
@@ -12,7 +12,7 @@
                                 lead.contactFullName }}</span>
                         </v-col>
                         <br>
-                        <span class="noPad caption grey--text">{{ lead.dateCreated }}</span>
+                        <span class="noPad caption grey--text">{{ getFormattedDate }}</span>
                     </v-row>
                 </v-col>
             </v-row>
@@ -21,7 +21,6 @@
 
             <v-divider></v-divider>
 
-            <!-- <v-card-text> -->
             <div class="mb-2 mt-2">
                 <v-row>
                     <v-col cols="12" sm="6" md="3" lg="3">
@@ -39,7 +38,6 @@
                         <span class="pl-4 grey--text">Lead Invitation</span></v-col>
                 </v-row>
             </div>
-            <!-- </v-card-text> -->
             <v-divider></v-divider>
             <div v-if="lead.statusLeadId === 1" class="pt-2 pb-2">
                 <v-row>
@@ -66,13 +64,13 @@
                 <div class="mt-2 pl-4 pb-2">
                     <v-row>
                         <v-col cols="12" sm="6" md="2" lg="2">
-                            <v-btn class="pl-4 white--text" depressed color="#FF7B19" @click="mudarStatus(1)">
+                            <v-btn class="pl-4 white--text" depressed color="#FF7B19" @click="changeStatus(1)">
                                 Accept
                             </v-btn>
                         </v-col>
                         <v-col cols="12" sm="6" md="2" lg="2">
 
-                            <v-btn class="pl-4 black--text" depressed color="#d3d3d3" @click="mudarStatus(2)">
+                            <v-btn class="pl-4 black--text" depressed color="#d3d3d3" @click="changeStatus(2)">
                                 Decline
                             </v-btn>
                         </v-col>
@@ -109,7 +107,26 @@ export default class LeadCard extends Vue {
     @namespaces.Action(LeadActionTypes.GET_INVITED)
     public getLeadInvited!: (request: string) => Promise<void>;
 
-    public async mudarStatus(status: number) {
+
+    public get getFormattedDate(){
+        return this.formatarData(this.lead.dateCreated.toString());
+    }
+
+    public formatarData(dataString: string): string {
+    const data = new Date(dataString);
+
+    const options: Intl.DateTimeFormatOptions = {
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    };
+
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(data).replaceAll('at','@');
+        return formattedDate;
+}
+    public async changeStatus(status: number) {
         let leadChanged: LeadModel = this.lead;
         leadChanged.statusLeadId = status;
         const endpoint = this.endpoint? '/cqrs' : ''
